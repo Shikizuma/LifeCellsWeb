231ca10a11643257f2b1361775df32b4b73962cc
Miroslav <myroslav.bashkirov@gmail.com>
2023-07-27 14:28:15 +0300

    Add vue logic, css style, and info on click on cells

--- LifeCellsWeb/Interface/app.js
+++ LifeCellsWeb/Interface/app.js
@@ -1,9 +1,33 @@
 ﻿let host = window.chrome.webview.hostObjects.apiwebcontroller;
 
-setTimeout(() => {
-    host.Test();
-}, 1);
-
 host.addEventListener('webmessage', event => {
     console.log(event)
 });
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        currentCell: null,
+        cellList: [
+            {
+                id: 1,
+                category: "Категория 1",
+                name: "Клетка 1"
+            },
+            {
+                id: 2,
+                category: "Категория 2",
+                name: "Клетка 2"
+            },
+        ],
+    },
+    methods: {
+        viewDetails() {
+           this.currentCell = this.cellList[0];
+        },
+        hideDetails() {
+            this.currentCell = null;
+        },
+    },
+   
+})
